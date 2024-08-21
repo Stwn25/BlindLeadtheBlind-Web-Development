8e909c27fa333d3c99da21397e32d1d1a9bcc84f
Stwn25 <msetiawanwibisono@gmail.com>
2024-08-21 13:07:30 +0700

    add icon navbar

--- Katalog Page/katalog.js
+++ Katalog Page/katalog.js
@@ -1,5 +1,115 @@
-const user = document.querySelector('.profile-user');
+let timer;
+const overlay = document.querySelector('.overlay');
 
-document.querySelector('#profile-click').onclick = () => {
-    user.classList.toggle('active');
-};
+// Icon profile 
+const profile = document.querySelector('.profile-user');
+
+//ketika mouseenter di icon profile
+document.querySelector('#profile-click').addEventListener('mouseenter', () => {
+    profile.classList.add('active');
+    overlay.style.display = profile.classList.contains('active') ? 'block' : 'none';
+});
+
+//ketika mouseleave dari icon profile
+document.querySelector('#profile-click').addEventListener('mouseleave', () => {
+    timer = setTimeout(() => {
+        profile.classList.remove('active');
+        overlay.style.display = 'none';
+    }, 150);
+});
+
+//ketika mouseenter di dropdown profile
+profile.addEventListener('mouseenter', () => {
+    clearTimeout(timer); // Menghapus timer jika mouse masuk lagi
+    overlay.style.display = profile.classList.contains('active') ? 'block' : 'none';
+});
+
+//ketika mouseleave dari dropdown profile
+profile.addEventListener('mouseleave', () => {
+    profile.classList.remove('active')
+    overlay.style.display = 'none';
+});
+
+
+
+// Icon keranjang
+const keranjang = document.querySelector('.keranjang-user');
+
+document.querySelector('#keranjang-click').addEventListener('mouseenter', () => {
+    keranjang.classList.add('active');
+    overlay.style.display = keranjang.classList.contains('active') ? 'block' : 'none';
+});
+
+//ketika mouseleave dari icon keranjang
+document.querySelector('#keranjang-click').addEventListener('mouseleave', () => {
+    timer = setTimeout(() => {
+        keranjang.classList.remove('active');
+        overlay.style.display = 'none';
+    }, 150);
+});
+
+//ketika mouseenter di dropdown keranjang
+keranjang.addEventListener('mouseenter', () => {
+    clearTimeout(timer); // Menghapus timer jika mouse masuk lagi
+    overlay.style.display = keranjang.classList.contains('active') ? 'block' : 'none';
+});
+
+//ketika mouseleave dari dropdown keranjang
+keranjang.addEventListener('mouseleave', () => {
+    keranjang.classList.remove('active')
+    overlay.style.display = 'none';
+});
+
+
+
+//Icon notification
+const notification = document.querySelector('.notif-user');
+const bt_transaksi = document.querySelector('.bt-transaksi');
+const bt_update = document.querySelector('.bt-update');
+const transaksi = document.querySelector('.transaksi')
+const update = document.querySelector('.update')
+
+document.querySelector('#notif-click').addEventListener('mouseenter', () => {
+    notification.classList.add('active');
+    overlay.style.display = notification.classList.contains('active') ? 'block' : 'none';
+    update.style.display = 'none';
+    bt_transaksi.classList.add('active');
+});
+
+//ketika mouseleave dari icon notif
+document.querySelector('#notif-click').addEventListener('mouseleave', () => {
+    timer = setTimeout(() => {
+        notification.classList.remove('active');
+        overlay.style.display = 'none';
+        bt_update.classList.remove('active')
+    }, 150);
+});
+
+//ketika mouseenter di dropdown notif
+notification.addEventListener('mouseenter', () => {
+    clearTimeout(timer); // Menghapus timer jika mouse masuk lagi
+    overlay.style.display = notification.classList.contains('active') ? 'block' : 'none';
+});
+
+//ketika mouseleave dari dropdown notif
+notification.addEventListener('mouseleave', () => {
+    notification.classList.remove('active');
+    overlay.style.display = 'none';
+    bt_update.classList.remove('active')
+});
+
+
+//button transaksi dan update
+document.querySelector('#bt-transaksi').addEventListener('mousedown', () => { //'mousedown' kondisi ketika click
+    bt_transaksi.classList.add('active');
+    bt_update.classList.remove('active');
+    transaksi.style.display = 'block';
+    update.style.display = 'none';
+});
+
+document.querySelector('#bt-update').addEventListener('mousedown', () => {
+    bt_update.classList.add('active');
+    bt_transaksi.classList.remove('active');
+    transaksi.style.display = 'none';
+    update.style.display = 'block';
+});
